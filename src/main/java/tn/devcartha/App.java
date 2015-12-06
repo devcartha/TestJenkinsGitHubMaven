bf23e4e3bf8a7f3b19818f658e8260cb55e583ff
Souhail <chaouechi.souhail@gmail.com>
2015-12-06 14:12:04 +0100

    Initial Commit

---
+++ src/main/java/tn/devcartha/App.java
@@ -0,0 +1,13 @@
+package tn.devcartha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World ! This is CI Test" );
+    }
+}
