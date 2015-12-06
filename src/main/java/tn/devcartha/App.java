809a6f4e0fde240f321f0408bf4b7ace808eefd3
Souhail <chaouechi.souhail@gmail.com>
2015-12-06 16:22:14 +0100

    TG-2 #closed

--- src/main/java/tn/devcartha/App.java
+++ src/main/java/tn/devcartha/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World ! This a task management test" );
+        System.out.println( "Hello World ! This a Task management Test" );
     }
 }
